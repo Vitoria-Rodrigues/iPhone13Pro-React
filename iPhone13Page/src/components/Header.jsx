99461a0f514fbc3e88abdafadb6f1661a7104561
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2026-05-04 20:21:36 -0300

    Feat: criar componente Header com navegação estática

---
+++ iPhone13Page/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import Apple from "../assets/img/logo_apple.svg";
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+        <nav>
+            <ul>
+                <li>
+                    <img src={Apple} alt="Apple Icon" />
+                </li>
+                <li>Loja</li>
+                <li>Mac</li>
+                <li>iPad</li>
+                <li>iPhone</li>
+                <li>Watch</li>
+                <li>AirPods</li>
+                <li>Só na Apple</li>
+                <li>Acessórios</li>
+                <li>Suporte</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
