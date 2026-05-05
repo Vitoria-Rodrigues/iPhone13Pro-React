79f4c6e9b8c81fa6ced889319be65bb5b8e372b7
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2026-05-05 19:54:19 -0300

    Feat: adiciona navegação estatica sobre as informações do card

---
+++ iPhone13Page/src/components/NaviPhone.jsx
@@ -0,0 +1,23 @@
+import './NaviPhone.css';
+
+const PromoBanner = () => {
+  return (
+    <div className="nav-iphone">
+        <div className="iphone-container">
+        <h3>iPhone 13 Pro</h3>
+        <div className="nav-container">
+            <div className="nav-promo">
+                <ul>
+                    <li>Visão Geral</li>
+                    <li>Mudar para o iPhone</li>
+                    <li>Especificações</li>
+                </ul>
+            </div>
+            <button className="btn">Comprar</button>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default PromoBanner
