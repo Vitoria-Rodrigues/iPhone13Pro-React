cb10e78b9e18a64b71147d12cfbf716f95af2970
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2026-05-05 19:59:11 -0300

    Fix: corrigi a fonte

---
+++ iPhone13Page/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Header from '../components/Header';
+import NaviPhone from '../components/NaviPhone';
+import PromoBanner from '../components/PromoBanner';
+import Card from '../components/Card';
+
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <NaviPhone />
+        <PromoBanner />
+        <Card />
+    </div>
+  )
+}
+
+export default Home
