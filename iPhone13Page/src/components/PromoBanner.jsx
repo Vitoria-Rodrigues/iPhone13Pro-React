6453cc56463dd295a79158e6d0c082e6eac2127c
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2026-05-05 19:55:36 -0300

    Feat: adiciona componente de promoção

---
+++ iPhone13Page/src/components/PromoBanner.jsx
@@ -0,0 +1,11 @@
+import './PromoBanner.css';
+
+const PromoBanner = () => {
+  return (
+    <div className='promo'>
+        <p>Pague seu novo iPhone em até 12 parcelas ou economize 10% nos pagamentos à vista. <a href="#">Saiba mais</a></p>
+    </div>
+  )
+}
+
+export default PromoBanner
